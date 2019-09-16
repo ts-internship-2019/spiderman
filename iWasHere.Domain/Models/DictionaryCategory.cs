476e70288abee0b3c7193421a7ed43eae2c9f83e
Ilona Cojoaca <ilonaa.cojoaca@gmail.com>
2019-09-16 13:58:48 +0300

    TI-11

---
+++ iWasHere.Domain/Models/DictionaryCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iWasHere.Domain.Models
+{
+    public partial class DictionaryCategory
+    {
+        public DictionaryCategory()
+        {
+            TouristAttraction = new HashSet<TouristAttraction>();
+        }
+
+        public int DictionaryCategoryId { get; set; }
+        public string DictionaryCategoryName { get; set; }
+
+        public virtual ICollection<TouristAttraction> TouristAttraction { get; set; }
+    }
+}
---
+++ iWasHere.Domain/Models/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iWasHere.Domain.Models
+{
+    public partial class Image
+    {
+        public int ImageId { get; set; }
+        public int TouristAttractionId { get; set; }
+        public string Path { get; set; }
+
+        public virtual TouristAttraction TouristAttraction { get; set; }
+    }
+}
