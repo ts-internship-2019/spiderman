eb17359783b915f865e4bb1fbd07f47dfdc5caf3
Alberto Tonegari <atonegari@totalsoft.ro>
2019-09-16 13:53:09 +0300

    (WIP) Change DatabaseContext

---
+++ iWasHere.Domain/Models/DictionaryCurrency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iWasHere.Domain.Models
+{
+    public partial class DictionaryCurrency
+    {
+        public DictionaryCurrency()
+        {
+            Currency = new HashSet<Currency>();
+            Ticket = new HashSet<Ticket>();
+        }
+
+        public int DictionaryCurrencyId { get; set; }
+        public string DictionaryCurrencyCode { get; set; }
+        public string DictionaryCurrencyName { get; set; }
+
+        public virtual ICollection<Currency> Currency { get; set; }
+        public virtual ICollection<Ticket> Ticket { get; set; }
+    }
+}
